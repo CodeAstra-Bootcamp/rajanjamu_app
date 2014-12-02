05c68c2c6b4eb429d3aaf051ee4a1480a5713b3c
Rajan Gupta <rajanjamu@gmail.com>
2014-12-02 18:33:36 +0530

    Edited routes and application html

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
-  get 'projects' => 'static_pages/projects'
-  get 'contact' => 'static_pages/contact'
+  get 'projects' => 'static_pages#projects'
+  get 'contact' => 'static_pages#contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
